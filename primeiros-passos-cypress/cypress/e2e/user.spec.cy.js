8a9bea43e5376ebc0d7cb802a7b237ff05c03a10
brunoluizgn <brunoluiz.gn@gmail.com>
2024-09-01 01:17:40 -0300

    add ramdon values on user spec

--- primeiros-passos-cypress/cypress/e2e/user.spec.cy.js
+++ primeiros-passos-cypress/cypress/e2e/user.spec.cy.js
@@ -4,6 +4,9 @@ import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyInfo from '../pages/myInfo'
 
+const Chance = require('chance')
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -12,15 +15,14 @@ const myInfo = new MyInfo()
 describe('Orange HRM Tests', () => {
 
     it('Info user update', () => {
-
       loginPage.accessLoginPage()
       loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
       dashboardPage.dashboardPage()
       menuPage.accessMyInfo()
-      myInfo.fillPersonalDetails("bruno", "luiz")
-      myInfo.fillEmployee()
+      myInfo.fillPersonalDetails(chance.first(), chance.last())
+      myInfo.fillEmployee("Employ", "OtherIdTest", "DriverTest", "2025-03-10")
       myInfo.fillNationality()
       myInfo.subimitButton()
-      
     })
+
 })
--- primeiros-passos-cypress/cypress/pages/myInfo.js
+++ primeiros-passos-cypress/cypress/pages/myInfo.js
@@ -3,16 +3,14 @@ class MyInfo {
     selectorsList()  {
 
         const selectors = {
-
             firstNameField: "[name='firstName']",
             lastNameField: "[name='lastName']",
             generiField: ".oxd-input--active",
             genericCombobox: ".oxd-select-text-input",
             secondItemCombobox:".oxd-select-dropdown > :nth-child(2)",
             thirdItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
-            dataField: "[placeholder='yyyy-dd-mm']",
+            dataField: ".oxd-date-input", 
             saveButton: ".oxd-button"
-
         }
 
         return selectors
@@ -20,39 +18,29 @@ class MyInfo {
     }
 
     fillPersonalDetails(firstName, lastName) {
-
         cy.get(this.selectorsList().firstNameField).clear().type(firstName)
         cy.get(this.selectorsList().lastNameField).clear().type(lastName)
-        
     }
 
-    fillEmployee () {
-
-        cy.get(this.selectorsList().generiField).eq(3).clear().type('Employ')
-        cy.get(this.selectorsList().generiField).eq(4).clear().type('OtherIdTest')
-        cy.get(this.selectorsList().generiField).eq(5).clear().type('DriverTest')
-        cy.get(this.selectorsList().dataField).eq(0).clear().type('2025-03-10')
-        cy.get(this.selectorsList().saveButton).eq(0).click()
-        cy.get('body').should('contain', 'Successfully Updated')
-
+    fillEmployee (Employ, OtherIdTest, DriverTest, DataLicense) {
+        cy.get(this.selectorsList().generiField).eq(3).clear().type(Employ)
+        cy.get(this.selectorsList().generiField).eq(4).clear().type(OtherIdTest)
+        cy.get(this.selectorsList().generiField).eq(5).clear().type(DriverTest)
+        cy.get(this.selectorsList().dataField).eq(1).clear().type(DataLicense)
     }
 
     fillNationality () {
-
         cy.get(this.selectorsList().genericCombobox).eq(0).click()
         cy.get(this.selectorsList().secondItemCombobox).click()  
         cy.get(this.selectorsList().genericCombobox).eq(1).click() 
         cy.get(this.selectorsList().thirdItemCombobox).click()
         cy.get(this.selectorsList().saveButton).eq(0).click()
         cy.get('body').should('contain', 'Successfully Updated')
-        
     }
     
     subimitButton() {
-
         cy.get(this.selectorsList().saveButton).eq(0).click()
         cy.get('body').should('contain', 'Successfully Updated')
-
     }
 }
 
